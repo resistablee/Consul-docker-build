f7ec3950b9f2df9963f8563b8a8d3d28713e3d64
Selman KOYAN <resistablee7@hotmail.com>
2025-09-20 01:13:49 +0300

    Update Consul Docker run command to use GetInterfaceIP

--- Consul docker build.sh
+++ Consul docker build.sh
@@ -1,3 +1,3 @@
 docker pull consul:1.15.4
 
-docker run -d --name=Consul -p 8500:8500 consul:1.15.4 agent -dev -client=0.0.0.0 -bind=0.0.0.0
+docker run -d --name=Consul -p 8500:8500 consul:1.15.4 agent -dev -client=0.0.0.0 -bind='{{GetInterfaceIP "eth0"}}'
